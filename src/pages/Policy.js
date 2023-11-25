37ea2a646cd080ab79d8d0e4edcac87210a54497
Mahfuz Rahman <mahfuzrahman517@gmail.com>
2023-11-25 20:21:09 +0600

    repolish policy

--- src/pages/Policy.js
+++ src/pages/Policy.js
@@ -12,10 +12,10 @@ const Policy = () => {
                         style={{ width: "100%" }}
                     />
                 </div>
-                <div className="col-md-4">
+                {/* <div className="col-md-4">
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto porro perspiciatis voluptatem sint quo non enim, dolores autem repellendus nobis eum ipsum esse sunt cupiditate vero nostrum, at magnam facilis dolorem repellat quos obcaecati? Minima corrupti temporibus nesciunt in voluptate, culpa laborum ea fuga praesentium similique labore error eveniet facere ullam aspernatur! Ducimus autem aspernatur nam, ex enim distinctio ipsam veniam minima corporis beatae dicta dignissimos quo hic eum, at iusto! Voluptatibus, animi iusto. Sequi molestias fugit qui labore? Hic fugit quaerat placeat maiores vero? Eveniet exercitationem possimus, expedita aspernatur vitae temporibus libero quibusdam repellendus, rem quod quas iste perspiciatis ea accusantium aperiam consequuntur cum molestias tenetur quae. Veniam distinctio vero dolores repellendus at natus et a optio, itaque fugiat in assumenda quas sequi necessitatibus tempore sint quis qui? Quisquam atque provident facilis nostrum autem amet neque harum aspernatur quibusdam molestias ad, placeat fugit, ea, voluptates vel ipsum? Accusantium eaque voluptate iusto quibusdam earum perspiciatis aspernatur? Saepe vel asperiores consectetur vitae atque similique, suscipit, neque placeat fuga distinctio, aperiam nisi soluta numquam corrupti eveniet dicta doloribus. Quas nisi eveniet repellendus quaerat, ad voluptate officiis perferendis repellat similique fugit vel, in at mollitia libero earum! Consequuntur eaque vel itaque incidunt exercitationem.</p>
 
-                </div>
+                </div> */}
             </div>
         </Layout>
     );
